f7bda0e1ab3098d714a007e2853db0dbd690e028
bhuvancom <bhuvancom@users.noreply.github.com>
2026-04-13 16:19:57 +0000

    web build from fix for web webdeploy bhuvancom/nikhat-Flutter@67e7fbfe338427f75ec8637109dd076a80d92c36

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "501782418" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "227316951" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
